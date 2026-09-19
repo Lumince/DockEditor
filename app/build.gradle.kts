0d1c08d241fdf9c49b5a758c81e291089a442e91
Lumince <jmrjmr1997@gmail.com>
2026-09-19 14:06:36 -0400

    Bump version 0.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lumi.dockeditor"
         minSdk = 32
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.1.7"
+        versionCode = 5
+        versionName = "0.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
